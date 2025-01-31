e10f978150aa0418e7d2e110ae8e8da36816dc99
Manasvini G <manasviniguhan24@gmail.com>
2025-01-31 13:10:54 +0530

    first

---
+++ src/main/java/com/manas/Demo/App.java
@@ -0,0 +1,27 @@
+package com.manas.Demo;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void addData(String data) {
+		System.out.print("Data"+ data);
+		if(data.equals("Manas")) {
+			System.out.print("Welcome");	
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void name(String data) {
+		System.out.print("Data"+ data);
+		if(data.equals("Manas")) {
+			System.out.print("Welcome");	
+		}
+	}
+
+}
