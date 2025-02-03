ce77f51d0aa63098a9ec40096c2eb65e7dcffb62
Manasvini G <manasviniguhan24@gmail.com>
2025-02-03 10:10:06 +0530

    new method

--- src/main/java/com/manas/Demo/Bank.java
+++ src/main/java/com/manas/Demo/Bank.java
@@ -1,1 +1,18 @@
 package com.manas.Demo;
+
+import java.util.Scanner;
+
+
+class Bank
+{
+	public static void main(String[] args) {
+		int a;
+		a= add(2,4);
+		System.out.println(a);
+	}
+
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	
+}
